bd49b6188b2b39c683128037f4a0f2561fe9fe43
Yvette Pettinelli <ipettinelli@att.net>
2017-09-28 11:16:32 -0500

    Add user accounts with Devise

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module MyBlog
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    # add for homework wk4 day3
+    config.time_zone = 'Central Time (US & Canada)'
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
